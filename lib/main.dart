4b9a27023c9af7b98eaa423950cdcb93eaa997f4
Trần Anh Quân <trananhquan2206>
2024-01-11 14:23:55 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:app_thuong_mai_dien_tu/views/account/acccount_view.dart';
 import 'package:app_thuong_mai_dien_tu/views/register/accountInfomation_view.dart';
 import 'package:app_thuong_mai_dien_tu/views/welcome/splash_view.dart';
+import 'package:app_thuong_mai_dien_tu/views/welcome/welcome_view.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  const Account()
+      home:  const AccountInformation()
       // Container(),
     );
   }
