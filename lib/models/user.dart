1752fd04d9651f12997e9a31eb85b67e7774a1b5
Trần Anh Quân <trananhquan2206>
2024-01-12 18:46:07 +0700

    update file user.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+class User{
+  final String uerName;
+  final String passWord;
+  final String fullName;
+  final String Address;
+  final String email;
+  final DateTime birthDay;
+  final String phoneNumber;
+  final String state;
+
+  User(this.uerName, this.passWord, this.fullName, this.email, this.birthDay, this.phoneNumber, this.state, this.Address);
+}
