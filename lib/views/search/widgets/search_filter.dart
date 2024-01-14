005beaf4e29b9569d0a281377585007c9c64ae12
unknown <nguyenhaovkag@gmail.com>
2024-01-13 23:27:45 -0800

    update file search_filter.dart

--- lib/views/search/widgets/search_filter.dart
+++ lib/views/search/widgets/search_filter.dart
@@ -42,105 +42,107 @@ class _SearchFilterState extends State<SearchFilter> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadiusDirectional.vertical(
-          top: Radius.circular(40),
-        ),
-      ),
-      height: MediaQuery.of(context).size.height / 1.238,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 10),
-          const Center(
-            child: Text(
-              "Bộ lọc",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadiusDirectional.vertical(
+            top: Radius.circular(40),
           ),
-          const SizedBox(height: 10),
-          const Divider(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 1.6,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FilterOption(
-                    lst: categorylst,
-                    nameOption: "Phân loại",
-                    onTap: checkOption,
-                  ),
-                  SizedBox(
-                    height: 151,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 16),
-                        const Text(
-                          'Khoảng giá',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                        const SizedBox(height: 16),
-                        Expanded(
-                          child: Column(
-                            children: [
-                              RangeSlider(
-                                mouseCursor: MaterialStateMouseCursor.clickable,
-                                activeColor: AppColor.primaryColor,
-                                min: 0,
-                                max: 100,
-                                values: rangeValues,
-                                onChanged: (value) {
-                                  setState(() {
-                                    rangeValues = value;
-                                    print(rangeValues);
-                                  });
-                                },
-                                divisions: 100,
-                                labels: RangeLabels(
-                                  rangeValues.start.round().toString(),
-                                  rangeValues.end.round().toString(),
-                                ),
-                              ),
-                              Text(
-                                'khoảng giá ${rangeValues.start} tr -- ${rangeValues.end} tr',
-                                style: const TextStyle(fontSize: 16),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  FilterOption(
-                    lst: sort,
-                    nameOption: "Sắp xếp theo",
-                    onTap: checkOption,
-                  ),
-                  const SizedBox(height: 16),
-                  const Text(
-                    'Đánh giá',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  const SizedBox(height: 16),
-                  ReviewOption(
-                    lst: ratelst,
-                    onTap: checkOption,
-                  ),
-                ],
+        ),
+        height: MediaQuery.of(context).size.height / 1.238,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 10),
+            const Center(
+              child: Text(
+                "Bộ lọc",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          const Divider(),
-          Container(
-            child: Row(
+            const SizedBox(height: 10),
+            const Divider(),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 1.63,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FilterOption(
+                      lst: categorylst,
+                      nameOption: "Phân loại",
+                      onTap: checkOption,
+                    ),
+                    SizedBox(
+                      height: 151,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 16),
+                          const Text(
+                            'Khoảng giá',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          const SizedBox(height: 16),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                RangeSlider(
+                                  mouseCursor:
+                                      MaterialStateMouseCursor.clickable,
+                                  activeColor: AppColor.primaryColor,
+                                  min: 0,
+                                  max: 100,
+                                  values: rangeValues,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      rangeValues = value;
+                                      print(rangeValues);
+                                    });
+                                  },
+                                  divisions: 100,
+                                  labels: RangeLabels(
+                                    rangeValues.start.round().toString(),
+                                    rangeValues.end.round().toString(),
+                                  ),
+                                ),
+                                Text(
+                                  'khoảng giá ${rangeValues.start} tr -- ${rangeValues.end} tr',
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    FilterOption(
+                      lst: sort,
+                      nameOption: "Sắp xếp theo",
+                      onTap: checkOption,
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      'Đánh giá',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    const SizedBox(height: 16),
+                    ReviewOption(
+                      lst: ratelst,
+                      onTap: checkOption,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Expanded(
@@ -157,8 +159,8 @@ class _SearchFilterState extends State<SearchFilter> {
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
