04f44540aec86bf2dbad1a76bcd1f873beee1034
fut <phucnguyen1922003@gmail.com>
2024-01-23 00:07:31 +0700

    fixed address

--- lib/views/account/account_view.dart
+++ lib/views/account/account_view.dart
@@ -192,7 +192,7 @@ class _AccountState extends State<Account> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (_) => const AddressView(),
+                  builder: (_) => AddressView(user: widget.user),
                 ),
               );
             },
